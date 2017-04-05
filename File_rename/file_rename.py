be0d079aa5d2970ce387086499b97547e3af4aac
Satyam <satyambhalla17@gmail.com>
2017-04-05 18:42:09 +0530

    Update file_rename.py

--- File_rename/file_rename.py
+++ File_rename/file_rename.py
@@ -3,17 +3,17 @@ import re
 
 def rename_files():
     #get file names from a folder
-    file_names = os.listdir(r"W:/Udacity Nanodegree/prank")
+    file_names = os.listdir(r"Path to the text file")
     #print (file_names)
     save_path = os.getcwd()
     print("Current working directory is "+save_path)
-    os.chdir(r"W:/Udacity Nanodegree/prank")
+    os.chdir(r"Path to text files")
     saved_path = os.getcwd()
     print (saved_path)
     #Rename each file
     for file_name in file_names:
         print("Old Name - "+file_name)
-        print("New Name - "+file_name.strip("0123456789"))
-        os.rename(file_name,file_name.strip("0123456789"))
+        print("New Name - "+file_name.strip("0123456789")) 
+        os.rename(file_name,file_name.strip("0123456789")) #Remove digits from the file names
     os.chdir(save_path)
 rename_files()
