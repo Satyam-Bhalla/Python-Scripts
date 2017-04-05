682cfaf19bf383a476bf61dc3cac5faadca2a90b
Satyam <satyambhalla17@gmail.com>
2017-04-05 18:49:18 +0530

    Update webbrowser.py
    This script can help users to take a break from their work after each hour 3 times and to play a youtube video for relaxation.

--- Web_browser/webbrowser.py
+++ Web_browser/webbrowser.py
@@ -3,7 +3,7 @@ import time
 
 total_breaks = 3
 break_count = 0
-
+#To take a break after 1 hour
 while(break_count < total_breaks):
     print("This program started on "+time.ctime())
     time.sleep(1*60*60)  
